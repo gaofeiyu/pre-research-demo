a7c71adba6739c73bf025fe0647a4726d5675330
gaofeiyu <feiyu.gao@gmail.com>
2020-07-08 20:21:53 +0800

    update

--- cfg/util/argv.js
+++ cfg/util/argv.js
@@ -1,28 +1,28 @@
 'use strict';
 
 module.exports = function() {
-  const argv = require('minimist')(process.argv.slice(2), {
-      string: ['host', 'port', 'env', 'target'],
-      boolean: ['release', 'debug', 'mpt', 'inline', 'mock', 'all', 'local', 'force'],
-      alias: {
-          r: 'release',
-          d: 'debug',
-          h: 'host',
-          p: 'port',
-          e: 'env',
-          a: 'all',
-          t: 'target',
-          i: 'inline',
-          m: 'mock',
-          l: 'local',
-          f: 'force',
-          https: 'https'
-      },
-      default: {
-          port: 8001,
-          env: 'local',
-          https: false
-      }
-  });
-  return argv;
+    const argv = require('minimist')(process.argv.slice(2), {
+        string: ['host', 'port', 'env', 'target'],
+        boolean: ['release', 'debug', 'mpt', 'inline', 'mock', 'all', 'local', 'force'],
+        alias: {
+            r: 'release',
+            d: 'debug',
+            h: 'host',
+            p: 'port',
+            e: 'env',
+            a: 'all',
+            t: 'target',
+            i: 'inline',
+            m: 'mock',
+            l: 'local',
+            f: 'force',
+            https: 'https'
+        },
+        default: {
+            port: 8001,
+            env: 'local',
+            https: false
+        }
+    });
+    return argv;
 };
